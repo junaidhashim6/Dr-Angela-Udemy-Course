323f9c916e814b78d8fc3c91d49b4c86d28124e2
Junaid Hashmi <junaidhashim6@gmail.com>
2022-05-11 17:22:10 +0500

    Completes the dice game challenge

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSourece= "images/" + randomDiceImage;
+var img1 = document.querySelectorAll("img")[0];
+img1.setAttribute("src", randomImageSourece);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSourece2= "images/" + randomDiceImage2;
+var img1 = document.querySelectorAll("img")[1];
+img1.setAttribute("src", randomImageSourece2);
+

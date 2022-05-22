de7ed7af90aa58e21e4b60e1781c40006b4f3635
Junaid Hashmi <junaidhashim6@gmail.com>
2022-05-22 17:57:41 +0500

    Add event listner to click buttons

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -2,6 +2,38 @@
 var count = document.querySelectorAll("button").length;
 for(var i=0; i<count; i++){
     document.querySelectorAll("button")[i].addEventListener("click", function (){
-        alert("I got clicked");
+        var buttonInnerHtml = this.innerHTML;
+        switch (buttonInnerHtml) {
+            case "w":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "s":
+                    var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+                    break;
+            case "d":
+                    var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+            case "j":
+                    var tom2 = new Audio("sounds/tom-2.mp3");
+                    tom2.play();
+                    break;
+            case "k":
+                    var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+                    break;
+            case "l":
+                    var tom4 = new Audio("sounds/tom-4.mp3");
+                    tom4.play();
+                    break;
+            default:
+                break;
+        }
     })
 }

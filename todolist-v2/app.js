c4f444fa7914fe87a9c8222c5fc8074598a53b1b
junaidhashim6 <junaidhashim6@gmail.com>
2022-09-25 18:12:36 +0500

    render database items into todolist

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -26,18 +26,30 @@ const item3 = new Item({
   name : "Welcome to your todolist!"
 });
 const defaultItems = [item1, item2, item3];
-Item.insertMany(defaultItems, function(err){
-  if (err){
-    console.log(err);
-  } else{
-    console.log("Succesfuly saved default items to DB")
-  }
-})
+
 
 app.get("/", function(req, res) {
 
+  
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err,foundItems){
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if (err){
+          console.log(err);
+        } else{
+          console.log("Succesfuly saved default items to DB")
+        }
+      });
+      res.redirect("/");
+    } else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+  
+    }
+  });
+
+
+  
 
 });
 

91b8218826858e4ccc780c889a2d95f18f8b92ef
Junaid Hashmi <junaidhashim6@gmail.com>
2022-11-09 11:25:45 +0500

    Fetche all the articles in db using get method

--- Wiki_API/app.js
+++ Wiki_API/app.js
@@ -14,12 +14,22 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB",{userNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
 const articleSchema = {
     title: String,
     content: String
 }
 const Article = mongoose.model("Article", articleSchema);
+app.get("/articles",function(req,res){
+  Article.find(function(err,foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }
+    else{
+      res.send(err);
+    }
+  });
+});
 //TODO
 
 app.listen(3000, function() {

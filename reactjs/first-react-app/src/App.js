80f5c0c95b8cf8c2d24e00a422dff27c6ef45267
junaidhashim6 <junaidhashim6@gmail.com>
2022-11-21 17:10:38 +0500

    Conditional rendering

--- reactjs/first-react-app/src/App.js
+++ reactjs/first-react-app/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import MyButton from './components/button';
 import  About  from './components/about';
+import AdminPanel from './components/admin';
+import LoginForm from './components/user';
+const isLoggedIn =false;
 function App() {
   return (
     <div className="App">
@@ -9,6 +12,13 @@ function App() {
       <h2>Welcom to My App</h2>
       <MyButton />
       <About />
+      <div>
+      {isLoggedIn ? (
+      <AdminPanel />
+      ) : (
+      <LoginForm />
+  )}
+     </div>
       </header>
     </div>
   );

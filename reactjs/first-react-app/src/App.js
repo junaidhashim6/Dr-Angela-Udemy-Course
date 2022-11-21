8db3bb88af22147639040d2a1291e7572f275dec
junaidhashim6 <junaidhashim6@gmail.com>
2022-11-21 15:37:07 +0500

    Create object and display it with jsx

--- reactjs/first-react-app/src/App.js
+++ reactjs/first-react-app/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+const user = {
+  name: "junaid",
+  desig:  "lead",
+  age:26
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <p>Name of the user is {user.name}</p>
+        <p>He is {user.age} years old</p>
+        <p>He works as {user.desig}</p>
       </header>
     </div>
   );

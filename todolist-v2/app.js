68592296ea3d0a01aa27c18fd444ffbd46977181
junaidhashim6 <junaidhashim6@gmail.com>
2022-11-29 01:00:46 +0500

    Add list items to DB

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -10,7 +10,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todoDB",{useNewUrlParser:true})
+
+mongoose.connect("mongodb://127.0.0.1:27017/todoDB", {useNewUrlParser:true})
 const itemsSchema = {
   name: String
 };
@@ -26,34 +27,39 @@ const item3 =new Item({
 });
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if(err){
-    console.log(err);
-  }
-  else{
-    console.log("Default items are added to DB");
-  }
-});
+
 
 app.get("/", function(req, res) {
 
+Item.find({},function(err,foundItems){
+  if(foundItems.length === 0){
+    Item.insertMany(defaultItems, function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("Default items are added to DB");
+      }
+    });
+    res.redirect("/");
+  }else{
+    res.render("list", {listTitle: "Today", newListItems:foundItems});
+  }
+})
 
-
-  res.render("list", {listTitle: "Today", newListItems:items });
+  
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name:itemName
+  })
+  item.save();
+  res.redirect("/");
 });
 
 app.get("/work", function(req,res){

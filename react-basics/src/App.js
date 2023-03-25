b93474838f498b567c157d401c6ecaef53461bb8
junaidhashim6 <junaidhashim6@gmail.com>
2023-03-25 14:32:21 +0500

    timer app using react hook

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  function increase() {
-    setCount(count + 1);
-  }
-  function decrease() {
-    setCount(count - 1);
+//   function increase() {
+//     setCount(count + 1);
+//   }
+//   function decrease() {
+//     setCount(count - 1);
+//   }
+//   return (
+//     <div className="container">
+//       <h1>{count}</h1>
+//       <button onClick={increase}>+</button>
+//       <button onClick={decrease}>-</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// react hook challange
+function App() {
+  setInterval(update, 1000);
+  let now = new Date().toLocaleTimeString();
+  const [time, Settime] = useState(now);
+
+  function update() {
+    let newTime = new Date().toLocaleTimeString();
+    Settime(newTime);
   }
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={decrease}>-</button>
+      <h1>{time}</h1>
+      <button onClick={update}>Get Time</button>
     </div>
   );
 }
